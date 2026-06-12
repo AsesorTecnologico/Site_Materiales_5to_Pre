9d0a1af03e4b7f0a8d586e3413df3ccc12b50617
AsesorTecnologico <77905882+AsesorTecnologico@users.noreply.github.com>
2026-06-11 21:01:43 -0500

    Update print statement from 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -723,7 +723,7 @@ const tomos = [
     { id: "tomo-i", name: "Tomo I", icon: "fas fa-book" },
     { id: "tomo-ii", name: "Tomo II", icon: "fas fa-book" },
     { id: "tomo-iii", name: "Tomo III", icon: "fas fa-book" },
-    //{ id: "tomo-iv", name: "Tomo IV", icon: "fas fa-book" },
+    { id: "tomo-iv", name: "Tomo IV", icon: "fas fa-book" },
     //{ id: "tomo-v", name: "Tomo V", icon: "fas fa-book" },
     //{ id: "tomo-vi", name: "Tomo VI", icon: "fas fa-book" },
     //{ id: "tomo-vii", name: "Tomo VII", icon: "fas fa-book" },
